978fb52178d80128d27ad561e60e7aeb3c709fea
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-06-21 21:09:28 -1000

    chirp*3

--- js/chirp.js
+++ js/chirp.js
@@ -1,7 +1,12 @@
 function chirp(n){
-  // FIX ME
+  if(n === 1){                              //n = 3 ; n = 2; n = 1 "chirp"
+    return "chirp";
+  }
+    return "chirp " + chirp(--n) ;          //"chirp" + chirp(2) ; "chirp" "chirp" ;
 }
 
 $(document).ready(function(){
   $("#result").html(chirp(3));
 });
+
+
